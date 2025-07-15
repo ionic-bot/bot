69109a1d0c46321f7a16f665b9dd96bae57c78fb
TinyBlueSapling <142849583+TinyBlueSapling@users.noreply.github.com>
2025-07-15 10:41:10 -0300

    Fix linting

--- src/commands/echo.ts
+++ src/commands/echo.ts
@@ -13,7 +13,12 @@ export default class implements ICommand {
 
     async run(_client: Client, interaction: ChatInputCommandInteraction): Promise<void> {
         if(process.env.DISCORD_TOKEN && process.env.CLIENT_ID) {
-            const emojis = (await new REST().setToken(process.env.DISCORD_TOKEN).get(Routes.applicationEmojis(process.env.CLIENT_ID)) as any).items;
+            const emojis = (await new REST().setToken(process.env.DISCORD_TOKEN).get(Routes.applicationEmojis(process.env.CLIENT_ID)) as {
+                items: {
+                    id: string,
+                    name: string
+                }[]
+            }).items;
             interaction.reply((interaction.options.getString('message') ?? '').replace(/:(?!<.*?>)(.*?):/g, (_, p1) => {
                 return `<:${emojis.find(x => x.name === p1)?.name}:${emojis.find(x => x.name === p1)?.id}>`;
             }));
